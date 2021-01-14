c09fd391c0ec41e25a32f80c59b11ba0cf6b0d10
Michael Vodep <mvodep@live.at>
2021-01-14 11:50:30 +0100

    Task 9

---
+++ ELK.Core/ELK.Core/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+
+namespace ELK.Core
+{
+    public class Foo
+    {
+        public string Prop1 { get; set; }
+        public string Prop2 { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            Foo f = new Foo
+            {
+                Prop1 = "AAAAA",
+                Prop2 = "BBBB"
+            };
+
+            Log.Error("{@Foo}", f);
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
