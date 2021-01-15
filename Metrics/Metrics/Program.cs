ff5cc6fcc034de523086908654e0f0fe30b53e1d
Michael Vodep <mvodep@live.at>
2021-01-15 14:27:19 +0100

    Task 10

---
+++ Metrics/Metrics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Prometheus;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Metrics
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
