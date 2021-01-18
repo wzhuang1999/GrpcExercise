03b63f2aea6d570f0405278454cc3723445175d2
Michael Vodep <mvodep@live.at>
2021-01-18 18:25:44 +0100

    Task 10

--- Metrics/Metrics/Program.cs
+++ Metrics/Metrics/Program.cs
@@ -7,10 +7,18 @@ namespace Metrics
 {
     class Program
     {
-
         static void Main()
         {
-            
+            for (int i = 0; i < 30; i++)
+            {
+                Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        await Task.Delay(100);
+                    }
+                });
+            }
 
             Console.ReadKey();
         }
